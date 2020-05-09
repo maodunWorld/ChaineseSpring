c5740d288f368f531744f4f03e61d76557841d8e
tongjian <tongjian@eefung.com>
2020-05-09 17:53:22 +0800

    删除

--- SpringRestful/rest/src/main/java/payroll/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package payroll;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-//继承Jpa接口， 指定实体类， 指定主键
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-}
--- SpringSecurity/myadmin/README.md
+++
@@ -1,4 +0,0 @@
-# SpringSecurity + Xadmin开发后台
-[xadmin](http://x.xuebingsi.com/index/down/index.html)
-
-TODO 
