5b6e792cad707a25da79ca11aff5d6e58c7b0931
maodunWorld <944126367@qq.com>
2020-05-13 15:57:52 +0800

    [UPDATE] 依赖优化

--- spring-boot-mongo-crud/src/main/java/com/maodun/mongo/config/MongoConf.java
+++ spring-boot-mongo-crud/src/main/java/com/maodun/mongo/config/MongoConf.java
@@ -1,11 +1,9 @@
 package com.maodun.mongo.config;
 
-import com.mongodb.client.MongoClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.MongoDbFactory;
 import org.springframework.data.mongodb.MongoTransactionManager;
-import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
 
 /**
  * @author maodunWorld
@@ -13,7 +11,7 @@ import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
  * @date 2020/5/13 13:20
  */
 @Configuration
-public class MongoConf  {
+public class MongoConf {
     @Bean
     public MongoTransactionManager transactionManager(MongoDbFactory dbFactory) {
         return new MongoTransactionManager(dbFactory);
