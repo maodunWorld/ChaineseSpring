14b8a81126bccf3eabdd42fa2b0a86e475540c12
maodunWorld <944126367@qq.com>
2020-06-19 17:58:15 +0800

    [ADD] 定时任务 日志引入， SpringBoot升级到2.3

---
+++ spring-boot-kafka/src/test/java/com/maodun/consumer/ListenerContainerTest.java
@@ -0,0 +1,17 @@
+package com.maodun.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.listener.ConsumerAwareMessageListener;
+
+/**
+ * @author tongjian
+ * @date 2020/6/19 17:35
+ */
+@SpringBootTest
+public class ListenerContainerTest {
+
+    @Test
+    void ContainerTest() {
+    }
+}
