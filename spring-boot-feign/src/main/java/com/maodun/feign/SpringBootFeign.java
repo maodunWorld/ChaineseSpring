785b903e0e1ed0931e5bc64feb0ea5274bc0fee1
maodunWorld <944126367@qq.com>
2020-06-22 10:58:51 +0800

    [ADD] Feign补充

--- spring-boot-feign/src/main/java/com/maodun/feign/SpringBootFeign.java
+++ spring-boot-feign/src/main/java/com/maodun/feign/SpringBootFeign.java
@@ -1,7 +1,27 @@
 package com.maodun.feign;
 
+import com.maodun.model.Pay;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.*;
 
-@FeignClient
+@FeignClient(name = "demo", url = "http://localhost:1888/api")
 public interface SpringBootFeign {
+
+    @GetMapping("/get")
+    public String get(@RequestParam String str1, @RequestParam String str2);
+
+    @GetMapping("/get/{str}")
+    public String getPath(@PathVariable String str);
+
+    @GetMapping("/getform")
+    public String getForm(@RequestParam String str);
+
+    @GetMapping("/getheader")
+    public String getHeader(@RequestHeader("test") String test);
+
+    @PostMapping("/post")
+    public String post(@RequestBody Pay pay);
+
+    @PostMapping("/postfrom")
+    public String postForm(@RequestParam("testform") String str);
 }
--- spring-boot-feign/src/test/java/com/maodun/feign/SpringBootFeignTest.java
+++ spring-boot-feign/src/test/java/com/maodun/feign/SpringBootFeignTest.java
@@ -1,12 +1,16 @@
 package com.maodun.feign;
 
+import com.maodun.FeignApp;
+import com.maodun.model.Pay;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(classes = FeignApp.class)
 @ActiveProfiles("feign")
+@Slf4j
 public class SpringBootFeignTest {
 
     @Autowired
@@ -14,6 +18,16 @@ public class SpringBootFeignTest {
 
     @Test
     void test1() {
+        String s = springBootFeign.get("hello", "hello");
+        log.info(s);
+    }
 
+    @Test
+    void test2() {
+        Pay pay = new Pay();
+        pay.setStr1("hello");
+        pay.setStr2("hello");
+        String post = springBootFeign.post(pay);
+        log.info(post);
     }
 }
