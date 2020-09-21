5af381573a6519a1ee9a5122fa42ceeed43755d1
maodunWorld <944126367@qq.com>
2020-09-21 19:52:55 +0800

    [PROJECT] 本地缓存项目完成

---
+++ spring-boot-local-cache/src/main/java/com/maodun/config/CacheBean.java
@@ -0,0 +1,28 @@
+package com.maodun.config;
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author tongjian
+ * @date 2020/9/21 19:51
+ */
+@Configuration
+public class CacheBean {
+    @Bean
+    public CaffeineCacheManager caffeineCacheManager() {
+        CaffeineCacheManager cacheManager = new CaffeineCacheManager();
+        cacheManager.setCaffeine(Caffeine.newBuilder()
+                // 设置最后一次写入或访问后经过固定时间过期
+                .expireAfterAccess(60, TimeUnit.SECONDS)
+                // 初始的缓存空间大小
+                .initialCapacity(100)
+                // 缓存的最大条数
+                .maximumSize(1000));
+        return cacheManager;
+    }
+}
