dd1aca6530c6190cdf0acd3c848efe3a4c3ea7c8
maodunWorld <944126367@qq.com>
2021-02-01 10:46:31 +0800

    [ADD] cache 构建

--- spring-boot-local-cache/src/main/java/com/maodun/LocalCacheApp.java
+++ spring-boot-local-cache/src/main/java/com/maodun/LocalCacheApp.java
@@ -1,8 +1,15 @@
 package com.maodun;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.Cache;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author tongjian
@@ -10,8 +17,24 @@ import org.springframework.cache.annotation.EnableCaching;
  */
 @SpringBootApplication
 @EnableCaching
-public class LocalCacheApp {
+public class LocalCacheApp implements CommandLineRunner {
     public static void main(String[] args) {
         SpringApplication.run(LocalCacheApp.class, args);
+    }
+
+    @Autowired
+    private CaffeineCacheManager caffeineCacheManager;
+
+    @Override
+    public void run(String... args) throws Exception {
+        Cache test = caffeineCacheManager.getCache("test");
+        HashMap<String, String> stringStringHashMap = new HashMap<>();
+        stringStringHashMap.put("kjk", "kjk");
+        test.put("test", stringStringHashMap);
+
+        Map<String, String> test1 = test.get("test", Map.class);
+
+        String kjk = test1.get("kjk");
+        System.out.println(kjk);
     }
 }
